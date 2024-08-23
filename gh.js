0cbc522301eabbd1ec9edd21a30f8a54c988ce39
Ahsaan0 <ahsanchamp009@gmail.com>
2024-08-23 10:43:46 +0500

    project

---
+++ gh.js
@@ -0,0 +1,23 @@
+alert("Ahsan Ali")
+alert("Wllcome to \n AhsaN World");
+
+let name=prompt("What is Your NAME");
+console.log(name);
+
+let passowrd=prompt("Enter Your Password");
+console.log(name);
+
+
+let weight=70;
+weight=weight+2;
+let weight2=80;
+var kiaHal="KIA HAL ";
+
+alert(kiaHal);
+
+let val1=80;
+let val2= 90;
+
+let sum = val1+val2;
+alert(sum);
+document.getElementById("FOOT").innerText =kiaHal 
